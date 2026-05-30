b95cbcdd5d5189bdd0e8d81cba6ba4b88826c510
Yoshita09 <yoshitasinghal.2005@gmail.com>
2026-05-31 03:40:46 +0530

    Database setup

---
+++ src/lib/supabase.ts
@@ -0,0 +1,18 @@
+import "react-native-url-polyfill/auto";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase env variables");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    storage: AsyncStorage,
+    persistSession: true,
+    autoRefreshToken: true,
+  },
+});
